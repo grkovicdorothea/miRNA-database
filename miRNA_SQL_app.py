16be324be5c1e65bfe20cfd09169e5429b585b1e
Dorothea Grkovic <135038649+grkovicdorothea@users.noreply.github.com>
2025-06-26 11:37:34 +0300

    Update miRNA_SQL_app.py

--- miRNA_SQL_app.py
+++ miRNA_SQL_app.py
@@ -46,43 +46,53 @@ if not os.path.exists(db_path):
 
 # ---------- Sidebar: CSV to Table Mapping ----------
 with st.sidebar:
-    st.markdown("### Core Tables")
-
-    with st.expander("core_mirna", expanded=False):
-        st.markdown("merged_mirBase")
-        st.markdown("miRstart_human_miRNA_information")
-
-    with st.expander("core_gene", expanded=False):
-        st.markdown("miRNet-mir-tf-hsa")
-        st.markdown("miRNet-mir-lncRNA")
-        st.markdown("miRNet-mir-pseudogene")
-        st.markdown("miRNet-mir-sncRNA")
-
-    with st.expander("core_disease", expanded=False):
-        st.markdown("HMDD")
-        st.markdown("dbDEMC_low_throughput")
-        st.markdown("miRcancer")
-
-    with st.expander("core_snp", expanded=False):
-        st.markdown("miRNASNPv4_pre-miRNA_variants")
-        st.markdown("miRNASNPv4_SNP_associations_multiCancer_celltype")
-        st.markdown("miRNet-snp-mir-hsa")
-        st.markdown("miRNet-snpmirbs-hsa")
-
-    with st.expander("core_drug", expanded=False):
-        st.markdown("ncDR_Curated_DRmiRNA")
-        st.markdown("ncDR_Predicted_DRmiRNA")
-        st.markdown("miRNASNPv4_drug_SNP_associations_multiCancer")
-        st.markdown("miRNet-mir-mol-hsa")
-
-    with st.expander("core_metadata", expanded=False):
-        st.markdown("miRNA_similarity_scores_ALL")
-        st.markdown("miRstart_human_miRNA_TSS_information")
-        st.markdown("plasmiR")
-
-    with st.expander("relationships", expanded=False):
-        st.markdown("miRNet-mir-epi-hsa")
-
+    st.markdown("### 🧬 Core Tables")
+    st.markdown("#### 🔹 core_mirna")
+    with st.expander("merged_mirBase"):
+        st.markdown("- \n- miRNA_ID\n- miRBase_acc\n- miRNA_sequence\n- miRNA_type")
+    with st.expander("miRstart_human_miRNA_information"):
+        st.markdown("- \n- miRNA_ID\n- miRNA_location\n- PCG\n- PCG_embl\n- lncRNA_embl\n- Intragenic/Intergenic\n- PCG_exon/intron\n- lncRNA_exon/intron")
+    st.markdown("#### 🔹 core_gene")
+    st.markdown("#### 🔹 core_disease")
+    with st.expander("HMDD"):
+        st.markdown("- \n- PMID\n- miRNA_ID\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    with st.expander("miRcancer"):
+        st.markdown("- \n- miRNA_ID\n- miRNA_expression\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    with st.expander("plasmiR"):
+        st.markdown("- \n- miRNA_ID\n- miRBase_acc\n- precursor_miRNA_id\n- PMID\n- diagnostic_marker\n- prognostic_marker\n- tested_prognostic_outcome\n- Biomarker_sample_type\n- miRNA_expression\n- Cell_line\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    st.markdown("#### 🔹 core_snp")
+    with st.expander("miRNet-snp-mir-hsa"):
+        st.markdown("- \n- miRNet_id\n- SNP_location\n- dbSNP_id\n- mature_miRNA_id\n- mature_miRBase_acc\n- miRNA_ID\n- miRBase_acc\n- miRNA_domain\n- SNP_High_Confidence\n- SNP_Robust_FANTOM5\n- Conserved_ADmiRE\n- AF_Percentile_gnomAD\n- Phastcons_100way")
+    with st.expander("miRNet-snpmirbs-hsa"):
+        st.markdown("- \n- miRNet_id\n- SNP_location\n- dbSNP_id\n- SNP_gene")
+    with st.expander("miRNASNPv4_SNP_associations_multiCancer_celltype"):
+        st.markdown("- \n- Cell_line\n- Immune_cell_abundance\n- beta\n- Pvalue\n- FDR\n- SNP_ref\n- SNP_alt\n- SNP_Source\n- SNP_location\n- dbSNP_id\n- SNP_gene\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    with st.expander("miRNASNPv4_pre-miRNA_variants"):
+        st.markdown("- \n- SNP_location\n- dbSNP_id\n- SNP_ref\n- SNP_alt\n- miRNA_ID\n- deltaG\n- miRNA_domain")
+    st.markdown("#### 🔹 core_drug")
+    with st.expander("miRNet-mir-mol-hsa"):
+        st.markdown("- \n- miRNet_id\n- miRBase_acc\n- miRNA_ID\n- Drug\n- CID\n- SMILES\n- Cell_line\n- PMID\n- miRNA_expression")
+    with st.expander("ncDR_Curated_DRmiRNA"):
+        st.markdown("- \n- PMID\n- miRNA_ID\n- miRBase_acc\n- Drug\n- CID\n- SMILES\n- miRNA_expression\n- Drug_effect\n- Target_gene\n- Regulation\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    with st.expander("ncDR_Predicted_DRmiRNA"):
+        st.markdown("- \n- NSC_ID\n- Drug\n- CID\n- SMILES\n- miRNA_ID\n- miRBase_acc\n- Pvalue\n- Qvalue\n- logFC\n- miRNA_expression\n- Drug_effect_size\n- Drug_effect")
+    with st.expander("miRNASNPv4_drug_SNP_associations_multiCancer"):
+        st.markdown("- \n- Drug\n- CID\n- SMILES\n- Drug_effect\n- beta\n- Pvalue\n- FDR\n- SNP_ref\n- SNP_alt\n- SNP_Source\n- SNP_gene\n- SNP_location\n- dbSNP_id\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    st.markdown("#### 🔹 core_dataset")
+    with st.expander("dbDEMC_low_throughput"):
+        st.markdown("- \n- miRNA_ID\n- Cell_line\n- miRNA_expression\n- ExperimentSourceInfo\n- PMID\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    with st.expander("miRNet-mir-epi-hsa"):
+        st.markdown("- \n- miRNet_id\n- miRBase_acc\n- miRNA_ID\n- epi_regulator\n- epi_modification\n- miRNA_expression\n- PMID\n- epi_target\n- Disease\n- Disease_MESH_ID\n- Disease_DOID_ID\n- Disease_categories\n- Disease_main_type\n- Disease_sub_type")
+    with st.expander("miRNet-mir-tf-hsa"):
+        st.markdown("- \n- miRNet_id\n- miRBase_acc\n- miRNA_ID\n- TF_gene\n- TF_gene_entrez\n- TF_gene_embl\n- TF_action_type\n- PMID")
+    with st.expander("miRNet-mir-lncRNA"):
+        st.markdown("- \n- miRNet_ID\n- miRBase_acc\n- miRNA_ID\n- lncRNA_gene \n- lncRNA_entrez\n- lncRNA_embl")
+    with st.expander("miRNet-mir-pseudogene"):
+        st.markdown("- \n- miRNet_ID\n- miRBase_acc\n- miRNA_ID\n- pseudogene\n- pseudogene_entrez\n- pseudogene_embl")
+    with st.expander("miRNet-mir-sncRNA"):
+        st.markdown("- \n- miRNet_ID\n- miRBase_acc\n- miRNA_ID\n- snc_gene\n- snc_entrez\n- snc_embl")
+    st.markdown("#### 🔹 core_publication")
+    st.markdown("#### 🔹 core_cell_line")
 # ---------- SQL Query Interface ----------
 st.markdown("### Run SQL Query")
 
